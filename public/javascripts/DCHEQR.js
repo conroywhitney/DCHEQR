a80724ab088323e41b96af0986bd6910ac48907c
Conroy Whitney <conroy.whitney@gmail.com>
2011-06-30 18:58:44 -0400

    adding DonorsChoose API

--- public/javascripts/DCHEQR.js
+++ public/javascripts/DCHEQR.js
@@ -1,14 +1,41 @@
-$("#txtProjURL").change(function() {
-    var DC_URL = $(this).val();
-
+$("#projSubmit").click(function() {
+    var DC_URL = $("#projURL").val();
     // TODO: check that given URL matches what we expect in a DonorsChoose URL
-    //if (val.substring(0, 52) == 'http://www.donorschoose.org/donors/proposal.html?id=') {
-    DC_URL = encodeURIComponent(DC_URL);
+    //if (DC_URL.substring(0, 52) == 'http://www.donorschoose.org/donors/proposal.html?id=') {
 
-    $.get("http://api.bitly.com/v3/shorten?login=dcheqr&apiKey=R_d5fcb40ad8e5264d79035db8deb79107&format=txt&longUrl=" + DC_URL, displayQR);
+    var proj_id = DC_URL.substr(52, 8);
+    var encoded_url = encodeURIComponent(DC_URL);
+
+    $.ajax({
+        url: "http://api.donorschoose.org/common/json_feed.html",
+        type: "get",
+        data: {
+            'solrQuery': "id:" + proj_id,
+            'APIKey': 'DONORSCHOOSE'
+        },
+        dataType: "jsonp",
+        cache: false,
+        success: displayProject
+    });
+
+    $.get("http://api.bitly.com/v3/shorten?login=dcheqr&apiKey=R_d5fcb40ad8e5264d79035db8deb79107&format=txt&longUrl=" + encoded_url, displayQR);
 });
 
 function displayQR(response) {
     // TODO: check that bitly response matches what we would expect
-    $("#imgProjQR").attr("src", response + ".qrcode");
+    $("#projQR").attr("src", response + ".qrcode");
+}
+
+function displayProject(response) {
+    // TODO: check that response is correct
+    // TODO: check that has proposal
+    var project = response.proposals[0];
+    if (project) {
+        $("#projImage").attr("src", project.imageURL);
+        $("#projTitle").html(project.title);
+        $("#projSchool").html(project.schoolName);
+        $("#projDesc").html(project.shortDescription);
+        $("#projCity").html(project.city);
+        $("#projState").html(project.state);
+    }
 }
